39458d90e1ee5dce01967bb77cceaffbba5cce65
“Stefan” <vasea@vasea.com>
2022-05-13 21:25:31 +0300

    main - 2 commit

--- view.py
+++ view.py
@@ -1,9 +1,9 @@
 
 
-def print_hi(name):
+def print_hi1(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi1('PyCharm')
